2aab4bc88c0c362fffd354ec5f78674cd1f2aba1
4daliaomry4 <alaakhattab92@gmail.com>
2017-03-01 17:44:02 +0200

    make function to send to facebook, make test for response from facebook

--- google-sheet-api/utils.js
+++ google-sheet-api/utils.js
@@ -2,74 +2,60 @@ var qs = require('querystring');
 var https = require('https');
 var google = require('googleapis');
 var key = require('../key.json');
-
-
+var id=0;
 function parseBody(req, callback) {
     var body = '';
     req.on('data', function(data) {
         body += data;
+        console.log("123");
     });
     req.on('end', function() {
-        callback(undefined, qs.parse(body));
+    	var id1 = JSON.stringify(id);
+        var body1 = qs.parse(body);
+        
+        
+        var source = {
+            "range": "Sheet1",
+            "majorDimension": "ROWS",
+            "values": [
+                [body1.username, body1.phone,body1.key]
+            ]
+        }
+        callback(undefined, source);
     });
 }
 
+function writesheet(source) {
+    var jwtClient = new google.auth.JWT(
+        key.client_email,
+        null,
+        key.private_key, ['https://www.googleapis.com/auth/spreadsheets'],
+        null
+    );
 
-
-// var https = require('https');
-// var google = require('googleapis');
-// var key = require('../key.json');
-
-// var SHEET_ID = '1klLD4Wvfdb5fCwQUx0mbFqg2z1ZinuMZsTf5wO_YCgY';
-
-// var jwtClient = new google.auth.JWT(
-//     key.client_email,
-//     null,
-//     key.private_key, ['https://www.googleapis.com/auth/spreadsheets'],
-//     null
-// );
-
-// jwtClient.authorize((err, tokens) => {
-//     //console.log(tokens);
-//     if (err) {
-//         console.log(err);
-
-//         return;
-//     }
-
-
-
-//     var opts = {
-//         hostname: 'sheets.googleapis.com',
-//         port: 443,
-//         path: `/v4/spreadsheets/${SHEET_ID}/values/append?valueInputOption=USER_ENTERED`,
-//         method: 'POST',
-//         headers: {
-//             'Authorization': `Bearer ${tokens.access_token}`
-//         }
-//     };
-//     var req = https.request(opts, (res) => {
-//         //console.log(res.payload);
-//         var store = '';
-//         res.on('data', (chunk) => store = store + chunk);
-//         res.on('end', () => console.log('store', store));
-//     });
-
-//     var d = '{  "range": "",\
-//   "majorDimension": "USER_ENTERED",\
-//   "values": [\
-//     ["Item", "Cost", "Stocked", "Ship Date"],\
-//     ["aa", "$222.50", "0", "3/1/2016"],\
-//     ["z", "$15", "2", "3/15/2016"],\
-//     ["Eaaaangine", "$100", "1", "30/20/2016"],\
-//     ["Total2222s", "=SUM(B2:B4)", "=SUM(C2:C4)", "=MAX(D2:D4)"]\
-//   ], }'
-//     req.write(d);
-//     req.end();
-// });
-
-
+    var SHEET_ID = '1klLD4Wvfdb5fCwQUx0mbFqg2z1ZinuMZsTf5wO_YCgY';
+    jwtClient.authorize((err, tokens) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        var opts = {
+            hostname: 'sheets.googleapis.com',
+            port: 443,
+            path: `/v4/spreadsheets/${SHEET_ID}/values/Sheet1:append?valueInputOption=USER_ENTERED`,
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${tokens.access_token}`
+            }
+        };
+        var req = https.request(opts, (res) => {});
+        console.log(source);
+        req.write(source);
+        req.end();
+    });
+}
 module.exports = {
     parseBody: parseBody,
-    //creatsheet : creatsheet
+    writesheet: writesheet
+
 };
