0bd9084a095ee3d46ffab7ebc0c66d3d1808564a
SupervisionT <a.alshaikh82@gmail.com>
2017-03-02 01:10:46 -0800

    typo

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var http = require("http");
 var mainHandler = require("./main.js");
 var https = require('https');
 var google = require('googleapis');
-var key = require('./key.json');
 http.createServer(mainHandler).listen(process.env.PORT || 8080, function() {
     console.log('Listening on 8080');
 });
