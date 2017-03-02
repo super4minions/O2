178daac420010488677e23492b1cbfba31184d76
shahenaz Monia <shahy.m.93@gmail.com>
2017-03-02 07:44:59 +0200

    update style.css

--- google-sheet-api/router.js
+++ google-sheet-api/router.js
@@ -4,15 +4,12 @@ var utils1 = require('../facebook-api/utils1');
 var send = require('../facebook-api/send.js');
 
 module.exports = {
-  // 'GET /facebook.jpg':function (req, res) {
-  //   res.writeHead(200, {'Content-type' : 'image/jpg'});
-  //   var fileContents = fs.readFileSync('./front-end/img/facebook.jpg', {encoding: 'utf8'});
-  //   res.write(fileContents);
-  //   res.end();
-  // },
-  // "GET /": require("./front-end/img/facebook.jpg"),
-
-  // "GET /facebook.jpg": require("../front-end/img/facebook.jpg"),
+  'GET /style.css':function (req, res) {
+    res.writeHead(200, {'Content-type' : 'text/css'});
+    var fileContents = fs.readFileSync('./front-end/style.css', {encoding: 'utf8'});
+    res.write(fileContents);
+    res.end();
+  },
     "GET /": require("../google-sheet-api/index.js"),
     //"GET /facebook" : require('../app')}
     "POST /sheet": require("../google-sheet-api/sheet.js"),
