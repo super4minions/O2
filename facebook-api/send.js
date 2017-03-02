9605b83081b8f55caea8cbfb91ae691b1dcc3eb1
4daliaomry4 <alaakhattab92@gmail.com>
2017-03-02 14:05:51 +0200

    edit

--- facebook-api/send.js
+++ facebook-api/send.js
@@ -2,59 +2,75 @@
 var https = require('https');
 var utils1 = require('./utils1.js');
 var write = require('../google-sheet-api/utils.js');
+require('dotenv').config();
+var qs = require('querystring');
+var https = require('https');
+var google = require('googleapis');
 
-module.exports = function(fbid){
 
-var key = Math.floor((Math.random() * 100000) + 1);
-var source = {
-    "range": "Sheet1",
-    "majorDimension": "ROWS",
-    "values": [
-        [fbid, key]
-    ]
-}
-write.writesheet(JSON.stringify(source));
-console.log(source);
-var postData = JSON.stringify({
-  recipient:{id:fbid},
-  "message":{
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"button",
-        "text":`Welcom to Gaza Sky Geeks and thank You for your intrest in Bootcamp. For regestration please fill the following form and use this key: ${key}`,
-        "buttons":[
-          {
-            "type":"web_url",
-            "url":"https://docs.google.com/forms/d/e/1FAIpQLSdMW42u_uLkOLQLguE94e89V1NJ0lFFzCdWG1M0hXR9lUvGvQ/viewform",
-            "title":"Apply Now"
+module.exports = function(fbid, cb) {
 
-          }
+    var key1 = Math.floor((Math.random() * 100000) + 1);
+    
+    var source = {
+        "range": "Sheet1",
+        "majorDimension": "ROWS",
+        "values": [
+            [fbid, key1]
+        ]
+    };
+
+    var postData = JSON.stringify({
+        recipient: { id: fbid },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "button",
+                    "text": `Welcom to Gaza Sky Geeks and thank You for your intrest in Bootcamp. For regestration please fill the following form and use this key: ${key1}`,
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://docs.google.com/forms/d/e/1FAIpQLSdMW42u_uLkOLQLguE94e89V1NJ0lFFzCdWG1M0hXR9lUvGvQ/viewform",
+                        "title": "Apply Now"
+                    }]
+                }
+            }
         }
-      }
     });
-  // }
 
-var opts = {
-  hostname: 'graph.facebook.com',
-  port: 443,
-  path: `/v2.6/me/messages?access_token=EAAS3JTmMICoBAIIo62bwQQk1hWjITKEURA416OEZAiDo7PhgtWoCnVrMXCnKBKYhAP0GU5oSykpmh4kLTP2wXQZBXlVlO44hmIYkguTdINk5lHz7kTNFRffmuOmdsWktOlqvPjiLyKj3WLbWjPKpejksGpPnlcAP4A41LRJgZDZD`,
-  method: 'POST',
-  headers: {'Content-Type': 'application/json'}
-};
+    var opts = {
+        hostname: 'graph.facebook.com',
+        port: 443,
+        path: `/v2.6/me/messages?access_token=EAAS3JTmMICoBAIIo62bwQQk1hWjITKEURA416OEZAiDo7PhgtWoCnVrMXCnKBKYhAP0GU5oSykpmh4kLTP2wXQZBXlVlO44hmIYkguTdINk5lHz7kTNFRffmuOmdsWktOlqvPjiLyKj3WLbWjPKpejksGpPnlcAP4A41LRJgZDZD`,
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    };
 
-var req = https.request(opts, (res) => {
-  utils1.parseBody(res, (err, body) => {
-    if(err) {
-      console.log('err',err);
-      return res.end();
-    }
+    var req = https.request(opts, (res) => {
+        console.log('facebook reply');
+        utils1.parseBody(res, (err, body) => {
+            if (err) {
+                console.log('err parsin fb body reply', err);
+                cb(err,undefined);
+                return;
+            }
 
-    //console.log('body',body);
-  });
-});
+            write.writesheet(source, function (err, replyFromGoogle) {
+                if(err) {
+                    console.log('error writing to google sheet',err);
+                    cb(err,undefined);
+                    return;
+                }
+                cb(undefined, 'ok');
+            });
+        });
+    });
 
-req.write(postData);
-req.end();
+    req.on('error', function(err) {
+        console.log('error replying to FB',err);
+        cb(err,undefined);
+    })
 
+    req.write(postData);
+    req.end();
 }
--- tests/main.test.js
+++ tests/main.test.js
@@ -3,33 +3,22 @@
 var test = require('tape');
 var shot = require('shot');
 var mainHandler = require('../main.js');
-
-test('GET /:  should return index.html', t => {
+var send = require('../facebook-api/send.js');
+var utils1 = require('../facebook-api/utils1');
+test('GET /facebook => fb webhook should return challenge', t => {
     shot.inject(mainHandler, {
-            method: 'GET',
-            url: '/'
-        }
-
-    , res => {
-        var indexOf = res.payload.indexOf('form');
-        t.notEqual(indexOf, -1, 'got form somewhere in the index.html file');
-        t.equal(res.statusCode, 200, 'got 200 status code');
+        method: 'GET',
+        url: [
+            '/facebook?',
+            'hub.mode=subscribe',
+            '&hub.challenge=1753237040',
+            '&hub.verify_token=Test-API'
+        ].join('')
+    }, res => {
+        t.equal(res.payload, '1753237040', 'right challenge response');
         t.end();
     });
 });
-
-// test('POST /sheet:  shoud return not found', t => {
-//     shot.inject(mainHandler, {
-//             method: 'POST',
-//             url: '/sheet'
-//             //payload : 'username=alaa&phone=123654&data.key=10'
-//         }
-//     , res => {
-//     	console.log("REEE",res);
-//         t.equal(res.statusCode, 200, 'got 200 status code');
-//         t.end();
-//     });
-// });
 
 test('POST /facebook: should do something with the mess from FB', t => {
     shot.inject(mainHandler, {
@@ -37,6 +26,17 @@ test('POST /facebook: should do something with the mess from FB', t => {
         url: '/facebook',
         payload: { "object": "page", "entry": [{ "id": "1737409489904164", "time": 1488373892936, "messaging": [{ "sender": { "id": "1502992586398188" }, "recipient": { "id": "1737409489904164" }, "timestamp": 1488373892786, "message": { "mid": "mid.1488373892786:9216471367", "seq": 97283, "text": "bootcamp" } }] }] }
     }, res => {
+        console.log('boom');
         t.end();
     });
 });
+test('GET /notFound => should return simple "Not found" mess', t => {
+    shot.inject(mainHandler, {
+        method: 'GET',
+        url: '/notFound'
+    }, res => {
+        t.equal(res.payload, 'not found', 'right mess');
+        t.end();
+    });
+});
+
